11a62087ad1eba288005f46b375abd530a9de536
Archie McKenzie <archiem@princeton.edu>
2022-04-28 18:16:58 +0100

    cleaned

--- test/Subscrypto.js
+++ test/Subscrypto.js
@@ -2,14 +2,13 @@ const Subscrypto = artifacts.require('Subscrypto.sol');
 
 contract('Subscrypto', function (accounts) {
 
-    it('Can new subscription be activated?', async function () {
+    it('Can a new subscription be activated and paid out?', async function () {
         const subscrypto = await Subscrypto.new();
         let me = accounts[0];
         let you = accounts[1];
-        subscrypto.send(web3.utils.toWei('100', "ether")).then(subscrypto.newSubscription(you, 1, 10));
-        assert(subscrypto.isActive(me, you));
+        subscrypto.newSubscription(you, 1, 10, {value: 100});
+        subscrypto.receiveSubscription(me, {from: you});
+        return assert(subscrypto.isActive(me, you));
     });
-
    
-
 });
