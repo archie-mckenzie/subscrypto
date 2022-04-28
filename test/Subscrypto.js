9e66b9fb69045bb96ee296a461e6f5dec3af42c5
Archie McKenzie <archiem@princeton.edu>
2022-04-28 21:31:34 +0100

    passing test 3

--- test/Subscrypto.js
+++ test/Subscrypto.js
@@ -16,13 +16,13 @@ contract('Subscrypto', function (accounts) {
         let you = accounts[1];
         subscrypto.newSubscription(you, 1, 10, {value: 100});
         return assert(subscrypto.receiveSubscription(me, {from: you}));
-    });
+    }); 
 
     it('Able to detect a function that doesn\'t exist?', async function () {
         const subscrypto = await Subscrypto.new();
-        let me = accounts[0];
-        let you = accounts[1];
-        let success = await subscrypto.isActive(me, you);
+        let a2 = accounts[2];
+        let a3 = accounts[3];
+        let success = await subscrypto.isActive(a2, a3);
         return assert.equal(success, false);
     });
 
