a705240291470e375550750b762df05177b44d7b
Archie McKenzie <archiem@princeton.edu>
2022-04-28 18:52:11 +0100

    added test 3

--- test/Subscrypto.js
+++ test/Subscrypto.js
@@ -17,5 +17,15 @@ contract('Subscrypto', function (accounts) {
         subscrypto.newSubscription(you, 1, 10, {value: 100});
         return assert(subscrypto.receiveSubscription(me, {from: you}));
     });
+
+    it('Can it return a function that doesnt exist?', async function () {
+        const subscrypto = await Subscrypto.new();
+        let me = accounts[0];
+        let you = accounts[1];
+        let success = await subscrypto.isActive(me, you);
+        assert.equal(success, false);
+        return;
+    });
+
    
 });
