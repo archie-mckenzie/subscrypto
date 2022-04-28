adf0d0fcdc09c66a10fc008da77d02b6ba46fcac
Archie McKenzie <archiem@princeton.edu>
2022-04-28 17:31:05 +0100

    unit tests 1.0

--- test/Subscrypto.js
+++ test/Subscrypto.js
@@ -1,11 +1,13 @@
 const Subscrypto = artifacts.require('Subscrypto.sol');
 
-contract('Subscrypto', () => {
+contract('Subscrypto', function (accounts) {
 
-    const subscrypto = await Subscrypto.new();
-
-    it('Activating new subscription', () => {
-        // await subscrypto.newSubscription();
+    it('Can new subscription be activated?', async function () {
+        const subscrypto = await Subscrypto.new();
+        let me = accounts[0];
+        let you = accounts[1];
+        subscrypto.send(web3.utils.toWei('100', "ether")).then(subscrypto.newSubscription(you, 1, 10));
+        assert(subscrypto.isActive(me, you));
     });
 
    
